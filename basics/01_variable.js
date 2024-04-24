27b16d64f46b0089e438bf086097985f79d66e4d
Tabishbostami <tabish.bostami042000@gmail.com>
2024-04-24 17:26:59 +0530

    update

---
+++ basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 14423
+let accountEmail = "tabis.bostami@gmail.com"
+var accountPassword = "9234923"
+accountCity =  "Bokaro"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+let name = "Tabish bostami"
+let age = 23
+let isLogedIn = false
+
+//number => 2 to power 53
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefined =>
+// Symbol => unique
+
+//object
+
+console.log(typeof("Tabish"))
