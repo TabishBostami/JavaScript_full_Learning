769adea3b6cf71fc8e9f4cb6c47ae70c183180c5
Tabishbostami <tabish.bostami042000@gmail.com>
2024-04-24 23:46:42 +0530

    add_file_comparisions

---
+++ basics/04_comparision.js
@@ -0,0 +1,35 @@
+// common comparisions
+
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+ 
+//******* some special Compressions ***********
+
+// console.log("2">1);
+// {
+ 
+    // Note:- AVOID THIS TYPE OF COMPARISIONS❌     
+
+      // ❌ console.log(null > 0);  //false 
+      // ❌ console.log(null == 0); //flase
+      // ❌ console.log(null >= 0); //true
+
+      /*The reason is that an equality check == and comparisons > < >= <= work differently.
+      Comparisons convert null to a number, treating it as 0.
+      That's whv (3) null >= 0 is true and (1) null > 0 is false. */
+
+      // ❌ console.log(undefined > 0);  //false
+      // ❌ console.log(undefined == 0); //false
+      // ❌ console.log(undefined > 0); //false 
+  
+      
+// }
+
+
+//Strict check === (it's not check only value it also the data type of value)
+
+// console.log("2" === 2);
+
