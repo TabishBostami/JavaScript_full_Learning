91400b70b467267cbd29a6d5cd1d1c135146e5dd
Tabishbostami <tabish.bostami042000@gmail.com>
2024-05-05 17:03:53 +0530

    06_strings

--- basics/06_string.js
+++ basics/06_string.js
@@ -2,3 +2,23 @@ const name = "Tabish"
 const age = 24;
 
 console.log(`Hello my name is ${name} and my age is ${age}`);
+
+const  gameName = new String('TabishBostami');
+
+console.log(gameName[0]); //T
+console.log(gameName.__proto__); //{} which is returns objects
+
+console.log(gameName.length);//13
+console.log(gameName.toUpperCase());//TABISHBOSTAMI
+console.log(gameName.indexOf("B"));//6
+console.log(gameName.charAt(4))//s
+
+const newString =gameName.substring(0,5)
+console.log(newString)//Tabis
+
+const anotherString = gameName.slice(4,-2)
+console.log(anotherString)//shBosta
+
+const newStringOne = "    tabish     ";
+console.log(newStringOne) //    tabish
+console.log(newStringOne.trim()); //tabish
