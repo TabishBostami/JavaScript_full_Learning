0b99916c8ebcb08d2cd34057bb234f0ce4fdc858
Tabishbostami <tabish.bostami042000@gmail.com>
2024-05-06 13:00:25 +0530

    07_nums

--- basics/07_nums_nad_maths.js
+++ basics/07_nums_nad_maths.js
@@ -12,3 +12,9 @@ const newNumber = 123.39833
 console.log(newNumber.toPrecision(2));//1.2e+2 ---priority is given befor decimal values
 console.log(newNumber.toPrecision(3));//123
 console.log(newNumber.toPrecision(4));//123.4
+
+
+const hundreds = 10000000;
+console.log(hundreds.toLocaleString());//10,000,000 --US standard
+console.log(hundreds.toLocaleString('en-IN'));//1,00,00,000 --Indian standard
+
