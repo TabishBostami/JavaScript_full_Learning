827293e1756df3980c877d2f73994f1cb4e6be2b
Tabish bostami <tabish.bostami042000@gamil.com>
2024-06-07 21:15:49 +0530

    arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,12 @@
+// Array
+
+const  myArr = [0,1,2,3,4,5,"Tabish"]
+const myArr2 = new Array(1,2,3,4,5)
+
+console.log(myArr2)  //[ 1, 2, 3, 4, 5 ]
+console.log(myArr) // [ 0, 1, 2, 3, 4, 5, 'Tabish' ]
+
+console.log(myArr[0]) //0
+console.log(myArr[6]) //Tabish
+
+// Array methods
