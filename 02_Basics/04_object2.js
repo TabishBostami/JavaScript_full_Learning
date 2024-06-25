7cb6de33a5b621ecd510a2e7936660f637f3f429
Tabishbostami <tabish.bostami042000@gmail.com>
2024-06-25 09:16:05 +0530

    object2

--- 02_Basics/04_object2.js
+++ 02_Basics/04_object2.js
@@ -48,3 +48,21 @@ const obj3 = { ...obj1, ...obj2 };
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn')); //true
 // console.log(tinderUser.hasOwnProperty('isLogged')); //false
+
+//************** Object Destructuring ********************
+
+const course={
+  coursename : "Js with Tabish",
+  price : "999",
+  courseInstructor :"Tabish"
+
+}
+
+//Normal method:
+// console.log(course.courseInstructor) // Tabish
+
+
+// Syntax:  const {name_of_key : aliasing} = Object_name
+const {courseInstructor: instructor} = course
+// console.log(instructor) // Tabish
+
