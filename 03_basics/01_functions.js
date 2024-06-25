cb66c722080b040fff43eb463c1f1c731fc121c1
Tabishbostami <tabish.bostami042000@gmail.com>
2024-06-25 16:36:43 +0530

    fuctions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,68 @@
+function sayMyName() {
+  console.log("T");
+  console.log("a");
+  console.log("b");
+  console.log("i");
+  console.log("s");
+  console.log("h");
+}
+
+// sayMyName()
+
+function addTwoNumber(number1, number2) {
+  console.log(number1 + number2);
+}
+// addTwoNumber(3,4) // 7
+// addTwoNumber(3,"4") // 34
+// addTwoNumber(3,"a") //3a
+
+//Syntax
+// function addTwoNumber(parameters){
+//   console.log(number1 + number2)
+// }
+
+//Call fuctions Syntax
+// functionName(arguments)
+// Example : addTwoNumber(1,2)
+
+// const result = addTwoNumber(2,3)
+// console.log(result) //undefined
+//because here fuction just print the value not return so to save this we have to return the result
+
+function addTwoNumbers(number1, number2) {
+  let result = number1 + number2;
+  console.log("sum =") //sum =
+  return result;
+  console.log("Tabish bostami"); // this console.log will never exicute because return statement is       the  last statement of fuction.
+}
+
+// const result = addTwoNumbers(2, 4);
+// console.log(result);
+
+function loginUserMessage(username){
+  if(username === undefined){
+    
+    return("Please enter user name")
+  }
+  return `${username} just logged in`
+}
+
+// console.log(loginUserMessage())
+
+//note:- in one fuction onlu one return can exicute then that fuction get termitnated
+
+//one more technique to handle this is 
+
+function loginUserMessage2(username = "Tabish"){
+  if(username === undefined){
+    
+    return("Please enter user name")
+  }
+  return `${username} just logged in`
+}
+
+console.log(loginUserMessage2())//Tabish
+console.log(loginUserMessage2("Bostami"))//Bostami
+
+//here you can see that there I have used default paramiter which is Tabish and i prints when no argument is passes but when you pass a argument then that gargumnet will be the value of username
+
